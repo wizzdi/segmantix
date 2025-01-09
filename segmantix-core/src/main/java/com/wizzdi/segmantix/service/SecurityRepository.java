56e4a7ceda5bc3a2727f2c027ab5770810ae6901
Asaf Ben Natan <asaf@wizzdi.com>
2025-01-09 12:43:23 +0200

    midway plainsql

--- segmantix-core/src/main/java/com/wizzdi/segmantix/service/SecurityRepository.java
+++ segmantix-core/src/main/java/com/wizzdi/segmantix/service/SecurityRepository.java
@@ -28,6 +28,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -35,6 +37,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SecurityRepository {
 
@@ -138,6 +141,14 @@ public class SecurityRepository {
 		}
 		Set<String> instanceGroupIds = instanceGroups.stream().map(f -> f.getId()).collect(Collectors.toSet());
 		return instanceGroupPath.get().in(instanceGroupIds);
+	}
+
+	public static <T> String permissionGroupPredicate( List<IInstanceGroup> instanceGroups,FieldPathProvider fieldPathProvider, AtomicReference<String> instanceGroupPath){
+		if(instanceGroupPath.get()==null){
+			instanceGroupPath.set(fieldPathProvider.getInstanceGroupPath(r, cb));
+		}
+		Set<String> instanceGroupIds = instanceGroups.stream().map(f -> f.getId()).collect(Collectors.toSet());
+		return in("",instanceGroupPath.get(),instanceGroupIds);
 	}
 
 
@@ -283,6 +294,188 @@ public class SecurityRepository {
 
 		predicates.add(cb.and(tenantIdPath.in(securityContext.tenants().stream().map(f->f.getId()).toList()),cb.or(securityPreds.toArray(new Predicate[0]))));
 
+	}
+
+
+	public <T > String getPlainSQLSecurityPredicate(String alias,String creatorIdPath,String idPath,String tenantIdPath,String typePath,Set<String> types, ISecurityContext securityContext) {
+		if (!requiresSecurityPredicates(securityContext)) {
+			return;
+		}
+		types.add(allTypesId);
+		String aliasPrefix=alias==null?"":"."+alias;
+		SecurityPermissions securityPermissions = getSecurityPermissions(securityContext,types);
+		List<String> securityPreds = new ArrayList<>();
+		AtomicReference<Path<String>> join = new AtomicReference<>(null);
+		securityPreds.add(eq(aliasPrefix,creatorIdPath,(securityContext.user().getId())));//creator
+		//user
+		SecurityPermissionEntry<IUser> user = securityPermissions.userPermissions();
+		if (!user.allowed().isEmpty()) {
+			securityPreds.add(in(aliasPrefix ,idPath,user.allowed().stream().map(f->f.id())));
+		}
+		List<SecuredHolder> userDenied = user.denied();
+		List<ITenant> allowAllTenantsWithoutDeny=new ArrayList<>(); // in the case of allow all tenants ,and no denies we can improve the query or avoid a bunch of ors and use an IN clause
+		if (specificUserTypePermissionRequired(user,userDenied)) {
+			Set<String> allowedTypes = user.allowedTypes().stream().map(f -> f.type()).collect(Collectors.toSet());
+			securityPreds.add(and(
+					in(aliasPrefix,tenantIdPath,securityContext.tenants().stream().map(f->f.getId())),
+					user.allowAll() ? truePred(): typePath!=null?in(aliasPrefix,typePath,allowedTypes):allowedTypes.contains(r.getJavaType().getCanonicalName())?truePred():falsePred(),
+					userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+					(user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join)))
+
+			));
+
+		}
+		else{
+			if(user.allowAll()){
+				allowAllTenantsWithoutDeny.addAll(securityContext.tenants());
+			}
+		}
+		if (!user.allowedPermissionGroups().isEmpty()) {
+			securityPreds.add(permissionGroupPredicate(user.allowedPermissionGroups(),fieldPathProvider,join));
+		}
+		//role
+		for (SecurityPermissionEntry<IRole> role : securityPermissions.rolePermissions()) {
+			IRole roleEntity = role.entity();
+			ITenant tenant = roleEntity.getTenant();
+			if (!role.allowed().isEmpty()) {
+				securityPreds.add(and(
+						in(aliasPrefix,idPath,role.allowed().stream().map(f->f.id())),
+						userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+						user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join))
+				));
+			}
+			if (specificRoleTypesPermissionRequired(role,userDenied,user)) {
+				Set<String> roleAllowedTypes = role.allowedTypes().stream().map(f -> f.type()).collect(Collectors.toSet());
+				List<String> roleDeniedIds = role.denied().stream().map(f -> f.id()).toList();
+				securityPreds.add(and(
+						eq(aliasPrefix,tenantIdPath, tenant.getId()),
+						role.allowAll() ? truePred(): typePath!=null?in(aliasPrefix,typePath,roleAllowedTypes):roleAllowedTypes.contains(r.getJavaType().getCanonicalName())?truePred():falsePred(),
+						userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+						role.denied().isEmpty() ? truePred(): not(in(aliasPrefix,idPath,roleDeniedIds)),
+						user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join)),
+						role.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(role.deniedPermissionGroups(),fieldPathProvider,join))
+
+
+
+				));
+			}
+			else{
+				if(role.allowAll()){
+					allowAllTenantsWithoutDeny.add(tenant);
+				}
+			}
+			if (!role.allowedPermissionGroups().isEmpty()) {
+				securityPreds.add(and(
+						permissionGroupPredicate(role.allowedPermissionGroups(),fieldPathProvider,join),
+						userDenied.isEmpty() ? truePred() : not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+						user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join))
+
+				));
+			}
+
+		}
+		//tenant
+		List<SecurityPermissionEntry<ITenant>> tenants = securityPermissions.tenantPermissions();
+
+		List<IInstanceGroup> tenantAllowedPermissionGroups = tenants.stream().map(f -> f.allowedPermissionGroups()).flatMap(f -> f.stream()).collect(Collectors.toList());
+		List<String> tenantAllowed = tenants.stream().map(f -> f.allowed()).flatMap(f -> f.stream()).map(f->f.id()).collect(Collectors.toList());
+
+		List<SecuredHolder> roleDenied = securityPermissions.rolePermissions().stream().map(f -> f.denied()).flatMap(f -> f.stream()).collect(Collectors.toList());
+
+		List<String> roleDeniedIds = roleDenied.stream().map(f -> f.id()).toList();
+
+		List<IInstanceGroup> rolePermissionGroupDenied = securityPermissions.rolePermissions().stream().map(f -> f.deniedPermissionGroups()).flatMap(f -> f.stream()).collect(Collectors.toList());
+		if (!tenantAllowed.isEmpty()) {
+			securityPreds.add(and(
+					in(aliasPrefix,idPath,tenantAllowed),
+					userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+					roleDenied.isEmpty() ? truePred() : not(in(aliasPrefix,idPath,roleDeniedIds)),
+					user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join)),
+					rolePermissionGroupDenied.isEmpty()?truePred():not(permissionGroupPredicate(rolePermissionGroupDenied,fieldPathProvider,join))
+
+
+			));
+		}
+		if (!tenantAllowedPermissionGroups.isEmpty()) {
+			securityPreds.add(and(
+					permissionGroupPredicate(tenantAllowedPermissionGroups,fieldPathProvider,join),
+					userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+					roleDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,roleDeniedIds))
+
+			));
+		}
+		for (SecurityPermissionEntry<ITenant> tenant : tenants) {
+			if (specificTenantTypesPermissionRequired(tenant,userDenied,roleDenied,user,rolePermissionGroupDenied)) {
+				ITenant tenantEntity = tenant.entity();
+				Set<String> allowedTenantTypes = tenant.allowedTypes().stream().map(f -> f.type()).collect(Collectors.toSet());
+				List<String> tenantDeniedIds = tenant.denied().stream().map(f -> f.id()).toList();
+				securityPreds.add(and(
+						eq(aliasPrefix,tenantIdPath, tenantEntity.getId()),
+						tenant.allowAll() ? truePred(): typePath!=null?in(aliasPrefix,typePath,allowedTenantTypes):allowedTenantTypes.contains(r.getJavaType().getCanonicalName())?truePred():falsePred(),
+						userDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,userDenied.stream().map(f->f.id()))),
+						roleDenied.isEmpty() ? truePred(): not(in(aliasPrefix,idPath,roleDeniedIds)),
+						tenant.denied().isEmpty() ? truePred(): cb.not(in(aliasPrefix,idPath,tenantDeniedIds)),
+						user.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(user.deniedPermissionGroups(),fieldPathProvider,join)),
+						rolePermissionGroupDenied.isEmpty()?truePred():not(permissionGroupPredicate(rolePermissionGroupDenied,fieldPathProvider,join)),
+						tenant.deniedPermissionGroups().isEmpty()?truePred():not(permissionGroupPredicate(tenant.deniedPermissionGroups(),fieldPathProvider,join))
+
+				));
+			}
+			else{
+				if(tenant.allowAll()){
+					allowAllTenantsWithoutDeny.add(tenant.entity());
+				}
+			}
+		}
+		if(!allowAllTenantsWithoutDeny.isEmpty()){
+			securityPreds.add(or(in(aliasPrefix,tenantIdPath,allowAllTenantsWithoutDeny.stream().map(f->f.getId()))));
+		}
+
+
+		return and(
+				in(aliasPrefix,tenantIdPath,securityContext.tenants().stream().map(f->f.getId())),
+				or(securityPreds)
+		);
+
+	}
+	private static String and(String... preds){
+		return "("+Arrays.stream(preds).collect(Collectors.joining(" and "))+")";
+	}
+
+	private static String or(String... preds){
+		return "("+Arrays.stream(preds).collect(Collectors.joining(" or "))+")";
+	}
+	private static String or(List<String> preds){
+		return "("+preds.stream().collect(Collectors.joining(" or "))+")";
+	}
+	private static String not(String pred){
+		return "not("+pred+")";
+	}
+
+	private static String eq(String aliasPrefix,String left,String right){
+		return aliasPrefix+left+"="+qoute(right);
+	}
+
+	private static String in(String aliasPrefix, String left, Collection<String> right){
+		return aliasPrefix+left+"in ("+right.stream().map(f->qoute(f)).collect(Collectors.joining(","))+")";
+	}
+	private static String in(String aliasPrefix, String left, Stream<String> right){
+		return aliasPrefix+left+"in ("+right.map(f->qoute(f)).collect(Collectors.joining(","))+")";
+	}
+	private static String in(String aliasPrefix, String left, String right){
+		return aliasPrefix+left+"in ("+right+")";
+	}
+
+	private String truePred() {
+		return "1=1";
+	}
+
+	private String falsePred() {
+		return "2=1";
+	}
+
+	private static String qoute(String f) {
+		return "'" + f + "'";
 	}
 
 	private <T> Set<String> getRelevantTypes(EntityManager em, From<?, T> r) {
